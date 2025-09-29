1995af4b6c77d744f3c157966009f634c329c43f
gotayjosue <gotayjosue@gmail.com>
2025-09-29 17:16:45 -0600

    OAuth added

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = new express.Router();
 const controllers = require('../controllers/mainController')
 const validate = require('../middleware/validate')
+const {isAuthenticated} = require('../middleware/authentication')
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('../swagger.json')
+const swaggerDocument = require('../swagger.json');
+const passport = require('passport');
 
 router.use('/api-docs', swaggerUi.serve)
 
@@ -13,15 +15,33 @@ router.get('/users', controllers.getAllUsers)
 router.get('/activities/:id', controllers.getActivityById)
 router.get('/users/:id', controllers.getUserById)
 router.get('/api-docs', swaggerUi.setup(swaggerDocument))
+router.get('/login', passport.authenticate('github'), (req, res) => {})
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+
+        req.session.destroy(function (err){
+            if (err) {
+                console.error("Error destroying session", err);
+            }
+
+            res.clearCookie('connect.sid')
+
+            res.redirect('/');
+        })
+        
+    });
+});
 
 
-router.post('/activities', validate.activityRules, validate.check, controllers.createActivity)
-router.post('/users', validate.userRules, validate.check, controllers.createUser)
+router.post('/activities', isAuthenticated, validate.activityRules, validate.check, controllers.createActivity)
+router.post('/users', isAuthenticated, validate.userRules, validate.check, controllers.createUser)
 
-router.put('/activities/:id', validate.activityRules, validate.check, controllers.updateActivity)
-router.put('/users/:id', validate.userRules, validate.check, controllers.updateUser)
+router.put('/activities/:id', isAuthenticated, validate.activityRules, validate.check, controllers.updateActivity)
+router.put('/users/:id', isAuthenticated, validate.userRules, validate.check, controllers.updateUser)
 
-router.delete('/activities/:id', controllers.deleteActivity)
-router.delete('/users/:id', controllers.deleteUser)
+router.delete('/activities/:id', isAuthenticated, controllers.deleteActivity)
+router.delete('/users/:id', isAuthenticated, controllers.deleteUser)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 const express = require('express');
+const env = require('dotenv').config()
 const app = express();
 const routes = require('./routes/routes');
 const pool = require('./models/database');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
+const passport = require('passport')
+const session = require('express-session')
+const GitHubStrategy = require('passport-github2').Strategy
 
 //Middleware
+
+app.use(session({
+    secret: 'MySessioSecret',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
+passport.use(new GitHubStrategy(
+    {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done){
+    return done(null, profile)
+}
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/',(req, res) => {res.send(req.session.user !== undefined? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: 'api-docs', session: false}),
+    (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+    });
 
 app.use(cors());
 
